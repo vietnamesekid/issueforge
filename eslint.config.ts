9f0943d14919a51ecdb38e48aa2af0f7fdfcf8a1
tranminhquang <tranminhquang4421@gmail.com>
2026-08-26 10:55:43 +0700

    chore: update ESLint configuration and update dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,9 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['**/dist/**', '**/node_modules/**', '**/*.d.ts'] },
-  ...tseslint.configs.recommended,
+export default defineConfig(
+  globalIgnores(['**/dist/**', '**/node_modules/**', '**/*.d.ts']),
+  tseslint.configs.recommended,
   {
     rules: {
       '@typescript-eslint/consistent-type-imports': 'error',
